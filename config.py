225cc2df2376733dd0ad97775ab1a2d8b849a3c7
root <root@clearfogbase>
2019-03-24 19:42:54 +0800

    new config item

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 class BasicConfig(object):
     DEBUG = True
+    NO_FASTCGI = False
     FASTCGI_SOCK = '/tmp/moviepool.sock'
     BYR_COOKIE = 'c_secure_uid=MTgyNzAz; c_secure_pass=e27cebdda92981ed700ee16cab8efa99; c_secure_ssl=bm9wZQ%3D%3D; c_secure_tracker_ssl=bm9wZQ%3D%3D; c_secure_login=bm9wZQ%3D%3D'
     DELUGE_HOST = '192.168.1.40'
